2bcb5d545477751fbc4433ac38dd4e2b094f8785
Segundo Paladines <segundopaladines@gmail.com>
2023-09-06 23:23:53 -0500

    Operadores Lógicos

---
+++ operadores_logicos.js
@@ -0,0 +1,23 @@
+console.log(4 < 7);
+console.log(4 < 1);
+console.log(4 < 4);
+console.log(4 == 7);
+console.log("_______________________________________\n");
+console.log (3 == 3); 
+console.log (3 === 3); 
+console.log (3 == "3"); 
+console.log (3 === "3");
+console.log (7 == "7");
+console.log (7 === "7");
+
+//
+var a = 1;
+var b = 2;
+
+var c = a = b;
+
+console.log(a);
+console.log(b);
+console.log(c);
+console.log("\n");
+console.log(16/2/4);
