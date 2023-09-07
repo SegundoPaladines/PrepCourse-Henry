7c6a8e2d1f940df6b658f45900972f48cf1854d2
Segundo Paladines <segundopaladines@gmail.com>
2023-09-06 23:01:22 -0500

    variables

---
+++ variables.js
@@ -0,0 +1,18 @@
+//String 
+var bootcamp = "henry";
+
+//numeros
+var numeroEntero = 17;
+var numeroNegativo = -4;
+var numeroDecimal = 11.75;
+
+//booleanos
+var estoyEntendiendo = true;
+var estoyAburrido = false;
+
+//undefined
+var cajon1;
+console.log(cajon1);
+
+//nulll
+var sinNada = null;
