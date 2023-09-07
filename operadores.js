063c83a494149d968fd26fbc40284d3c6fe83267
Segundo Paladines <segundopaladines@gmail.com>
2023-09-06 23:12:31 -0500

    Operadores

---
+++ operadores.js
@@ -0,0 +1,24 @@
+//Suma
+console.log(3 + 5);
+
+//Resta
+console.log(14 - 7);
+
+//Multiplicacion
+console.log(8 * 6);
+
+//Division
+console.log(30 / 9);
+
+//Módulo
+console.log(95 % 4);
+
+//Precedencia
+console.log(3 + 5 * 2 - 8);
+console.log((3 + 5) * 2 - 8);
+
+//Concatenar Strings
+console.log("Hola"+"Camilo");
+var nombre = "CAMILO";
+var saludo = "HOLA "+nombre;
+console.log(saludo);
