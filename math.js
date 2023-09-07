71d513e7dc877bf77573e3549669525df12fcd8f
Segundo Paladines <segundopaladines@gmail.com>
2023-09-07 00:13:51 -0500

    Biblioteca Math

---
+++ math.js
@@ -0,0 +1,31 @@
+//Potencia
+var a = Math.pow(2,3);
+console.log(a);
+
+//Redondeo
+a = Math.round(0.49);
+console.log(a);
+
+a = Math.round(0.5);
+console.log(a);
+
+a = Math.floor(5.93);
+console.log(a);
+
+a = Math.ceil(3.1);
+console.log(a);
+
+//maximos
+console.log(Math.max(1, 11, 3, 5 ,9));
+
+//minimos
+console.log(Math.min(1, 11, 3, 5 ,9));
+
+//numero aleatorio
+var r = Math.random();
+console.log(r);
+
+var z = r *100;
+
+//aleatorio entre 0 y 100
+console.log(z);
